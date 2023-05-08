02bbd01a5f125af2ad134af01f487ae2d4490fff
shubham-root <shubham.root@gmail.com>
2023-05-09 00:32:30 +0530

    type implementation structure modified

--- deps.ts
+++ deps.ts
@@ -1,5 +1,42 @@
 // @deno-types="npm:@types/lodash@4.14.194"
 export interface OnMessageCallback { (done: unknown): void}
+export interface SchemaOption {
+    title: string;
+    allowInput: boolean;
+    placeholder?: string;
+    types?: string[];
+}
+export interface Schema {
+    label: string;
+    options?: SchemaOption | SchemaOption[]
+}
+
+export interface PropInner {
+    category: string;
+    color: string;
+    component: string;
+    schema: Schema
+}
+export interface Properties {
+    [name: string]: PropInner;
+}
+
+export type Wires = string[][]
+export interface ChildWires {
+    in: string[][]
+    out: string[][]
+}
+export type Position = {
+    x: number
+    y: number
+    z: number
+}
+export interface Metadata {
+    position: Position;
+    step_id: string;
+    tmp_id: string;
+    prefix: string;
+}
 
 export {default as lodash} from 'npm:lodash@4.17.21'
 export {default as mayautils} from "./utils/index.ts"
--- src/Symbol.ts
+++ src/Symbol.ts
@@ -1,48 +1,11 @@
 
-import { OnMessageCallback } from "../deps.ts";
+import { OnMessageCallback, ChildWires, Properties, Metadata, Wires } from "../deps.ts";
 import utils from "../utils/index.ts";
 
-interface SchemaOption {
-    title: string;
-    allowInput: boolean;
-    placeholder?: string;
-    types?: string[];
-}
-interface Schema {
-    label: string;
-    options?: SchemaOption | SchemaOption[]
-}
-
-interface PropInner {
-    category: string;
-    color: string;
-    component: string;
-    schema: Schema
-}
-interface Properties {
-    [name: string]: PropInner;
-}
-
-type Wires = string[][]
-interface ChildWires {
-    in: string[][]
-    out: string[][]
-}
 interface Children {
     wires: ChildWires,
     symbols: Symbol[]
 };
-type Position = {
-    x: number
-    y: number
-    z: number
-}
-interface Metadata {
-    position: Position;
-    step_id: string;
-    tmp_id: string;
-    prefix: string;
-}
 
 class Symbol {
     id = "";
