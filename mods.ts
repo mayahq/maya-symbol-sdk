4912f812487a29d8a5a81aa24528d370888b1a2b
shubham-root <shubham.root@gmail.com>
2023-05-12 15:40:24 +0530

    required types exported

--- mods.ts
+++ mods.ts
@@ -3,3 +3,5 @@ export {default as exceptions} from './utils/exceptions.ts'
 //@ts-ignore: IDE error just ignore
 export {default as TypedInput} from './utils/typedInput.ts'
 export {default as generateId} from './utils/generateId.ts'
+
+export type {SymbolType, OnMessageCallback, PrimitiveTypes, ComponentTypes} from './src/Symbol.d.ts'
